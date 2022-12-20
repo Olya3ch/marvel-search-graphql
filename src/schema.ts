66f4ef4416eed338b0499229111f28c15e22513d
Olya Tricolici <oleatrik@gmail.com>
2022-12-20 14:34:08 +0200

    creates the first query searchMovies

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,25 @@
 import { graphql, buildSchema } from "graphql";
+import axios from "axios";
+
+const OMDBApiURL = `http://www.omdbapi.com/?apikey=8bf835a0`;
 
 export const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Movie {
+        Title: String
+        Year: String
+        imdbID: String
+        Poster: String
+    }
+    type Query {
+        hello: String
+        searchMovies(movieName: String): [Movie]
+    }
 `);
 
-export const rootValue = { hello: () => "Hello world!" };
+const searchMovies = async (args: { movieName: String }) => {
+  const { data } = await axios.get(OMDBApiURL + `&s=${args.movieName}`);
+  return data.Search;
+};
+const hello = () => "Hello world!";
+
+export const rootValue = { hello, searchMovies };
