40fbadad4da1462266e3407e24ba4a2899e674e0
Olya Tricolici <oleatrik@gmail.com>
2023-01-17 13:22:13 +0200

    writes test for comic

---
+++ src/__tests__/comic.test.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { characters } from "../graphql/resolvers";
+import { createTestApolloServer } from "./utils";
+
+const { objectContaining, arrayContaining } = expect;
+
+const queryData = {
+  query: `query Comic($comicId: Int) {
+    comic(id: $comicId) {
+      title
+      characters {
+        name
+      }
+    }
+  }
+  `,
+  variables: { comicId: 99901 },
+};
+
+describe("comic resolver", () => {
+  let server: any, url: string;
+
+  beforeAll(async () => {
+    ({ server, url } = await createTestApolloServer());
+  });
+
+  it("shows comic by id", async () => {
+    const response = await request(url).post("/").send(queryData);
+    expect(response.body.data?.comic).toEqual(
+      objectContaining({
+        title: "Captain Carter: Woman Out Of Time (Trade Paperback)",
+        characters: arrayContaining([
+          objectContaining({
+            name: "Peggy Carter (Captain Carter)",
+          }),
+        ]),
+      })
+    );
+  });
+
+  afterAll(async () => {
+    await server?.stop();
+  });
+});
