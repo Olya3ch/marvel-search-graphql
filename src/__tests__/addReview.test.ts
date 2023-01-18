11b2fcfb94eb9dcb65951ea84c3055c3c91ca3f6
Olya Tricolici <oleatrik@gmail.com>
2023-01-18 14:35:33 +0200

    writes test for mutation addReview

---
+++ src/__tests__/addReview.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import { connectToDatabase } from "../repository/db";
+import { createTestApolloServer } from "./utils";
+
+const queryData = {
+  query: `mutation addReview($comicId: Int, $review: String) {
+    addReview(comicId: $comicId, review: $review) {
+      review
+      comicId
+    }
+  }`,
+  variables: { comicId: 99901, review: "Wow" },
+};
+
+describe("addReview mutation", () => {
+  let server: any, url: string;
+
+  beforeAll(async () => {
+    ({ server, url } = await createTestApolloServer());
+    connectToDatabase();
+  });
+
+  it("should execute mutation", async () => {
+    const response = await request(url).post("/").send(queryData);
+    expect(response.body.data.addReview).toEqual({
+      review: "Wow",
+      comicId: 99901,
+    });
+  });
+
+  afterAll(async () => {
+    await server?.stop();
+  });
+});
--- src/graphQL/mutations.ts
+++ src/graphQL/mutations.ts
@@ -1,6 +1,9 @@
 import { Review } from "../repository/db";
 
-export const addReview = (args: { comicId: number; review: String }) => {
+export const addReview = (
+  _: any,
+  args: { comicId: number; review: String }
+) => {
   const review = new Review({
     comicId: args.comicId,
     review: args.review,
